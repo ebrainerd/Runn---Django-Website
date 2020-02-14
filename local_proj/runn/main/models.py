6c72f8dce0b36e4fa6ca97d59ba732e3ee0a0052
Parker Mitchell <parker.mitchell1@gmail.com>
2020-02-14 12:24:10 -0800

    yes

--- local_proj/runn/main/models.py
+++ local_proj/runn/main/models.py
@@ -11,3 +11,18 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
+class Run(models.Model):
+	title = models.CharField(max_length=100)
+	content = models.TextField()
+	distance = models.FloatField()
+	time = models.IntegerField()
+	date_posted = models.DateTimeField(default=timezone.now)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+	@property
+	def pace(self):
+		return round(self.time / self.distance, 2)
+
+	def __str__(self):
+		return self.title
--- local_proj/runn/main/urls.py
+++ local_proj/runn/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+# from .views import PostListView
 from . import views
 
 urlpatterns = [
     path('', views.home, name="main-home"),
+    # path('', PostListView.as_view(), name="main-home"),
     path('about/', views.about, name="main-about"),
 ]

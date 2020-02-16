77707107efdb34b8b69aeede7ab0f9e0d5daa273
Parker Mitchell <parker.mitchell1@gmail.com>
2020-02-15 16:16:14 -0800

    added CRUD for posts

--- local_proj/runn/main/models.py
+++ local_proj/runn/main/models.py
@@ -1,22 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField()
-	date_posted = models.DateTimeField(default=timezone.now)
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-	def __str__(self):
-		return self.title
-
-class Run(models.Model):
-	title = models.CharField(max_length=100)
-	content = models.TextField()
-	distance = models.FloatField()
-	time = models.IntegerField()
+	distance = models.FloatField(default=0.0)
+	time = models.IntegerField(default=0)
 	date_posted = models.DateTimeField(default=timezone.now)
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -26,3 +18,6 @@ class Run(models.Model):
 
 	def __str__(self):
 		return self.title
+
+	def get_absolute_url(self):
+		return reverse('post-detail', kwargs={'pk': self.pk})
--- local_proj/runn/main/views.py
+++ local_proj/runn/main/views.py
@@ -1,26 +1,68 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.http import HttpResponse
-from django.views.generic import ListView
+from django.views.generic import (
+	ListView, 
+	DetailView, 
+	CreateView,
+	UpdateView,
+	DeleteView
+)
 from .models import Post
-from .models import Run
 
 
-def home(request):
+def home(request): 
 	context = {
-		'posts': Run.objects.all()
+		'posts': Post.objects.all()
 	}
-
-	# context = {
-	# 	'runs': Run.objects.all()
-	# }
 
 	return render(request, 'main/home.html', context)
 
-# class PostListView(ListView):
-# 	model = Post
-# 	template_name = 'main/home.html'  # <app>/<model>_<viewtpye>.html
-# 	context_object_name = 'posts'
-# 	ordering = ['-date_posted']
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'main/home.html'  # <app>/<model>_<viewtpye>.html
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+	model = Post
+
+class PostCreateView(CreateView):
+	model = Post
+	fields = ['title', 'content', 'distance', 'time']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = Post
+	fields = ['title', 'content', 'distance', 'time']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+
+		return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Post
+	success_url = '/'
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+
+		return False
+
 
 
 def about(request):

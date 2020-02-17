cfc5da01daf0a8d10f85b63692b354f91fa0604f
lemarpopal <19434803+lemarpopal@users.noreply.github.com>
2020-02-16 23:16:52 -0800

    fixed merge conflicts

--- local_proj/runn/runn/settings.py
+++ local_proj/runn/runn/settings.py
@@ -79,6 +79,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+    'runn': {
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {
+            'read_default_file': 'my.cnf'
+        }
     }
 }
 

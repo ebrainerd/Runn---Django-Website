02ac6db3a2b83c59d25ecb24093b98bcf07347ed
Elliot Brainerd <elliotjbrainerd@gmail.com>
2020-02-16 21:51:03 -0800

    Finished user registration system

--- local_proj/runn/runn/settings.py
+++ local_proj/runn/runn/settings.py
@@ -28,8 +28,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
     'main.apps.MainConfig',
-    'users.apps.UsersConfig'
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'

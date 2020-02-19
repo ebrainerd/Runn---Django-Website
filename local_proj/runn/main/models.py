157f15b51aeadc7d441d4275e62d73cb4e1e34af
lemarpopal <19434803+lemarpopal@users.noreply.github.com>
2020-02-19 13:21:50 -0800

    working on extending user model

--- local_proj/runn/main/models.py
+++ local_proj/runn/main/models.py
@@ -3,7 +3,8 @@ from django.db import connections
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-import MySQLdb
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Post(models.Model):
@@ -23,3 +24,26 @@ class Post(models.Model):
 
 	def get_absolute_url(self):
 		return reverse('post-detail', kwargs={'pk': self.pk})
+
+# https://docs.djangoproject.com/en/3.0/topics/auth/customizing/#extending-the-existing-user-model
+# https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html#onetoone
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.TextField(max_length=50, blank=True)
+    last_name = models.TextField(max_length=50, blank=True)
+    email = models.TextField(max_length=75, blank=True)
+    tagline = models.TextField(max_length=250, blank=True)
+    location = models.TextField(max_length=100, blank=True)
+    user_type = models.IntegerField(default=0) # 0 is regular user, 1 is coach 
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+    
+
+
